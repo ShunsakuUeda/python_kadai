4c370329fed8d0a85559b2d0c6d5de9c45fff12a
植田俊作 <uedashunsaku@mbp.local>
2020-11-01 22:52:55 +0900

    python 課題取込み

--- search.py
+++ search.py
@@ -1,17 +1,40 @@
-
-### 検索ツールサンプル
-### これをベースに課題の内容を追記してください
-
+### csvを読み込むためにインポート
+import csv
 # 検索ソース
-source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+sources=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 ### 検索ツール
 def search():
-    word =input("鬼滅の登場人物の名前を入力してください >>> ")
-    
-    ### ここに検索ロジックを書く
+    input_word =input("鬼滅の登場人物の名前を入力してください >>> ")
+    result = 0
+    ### 課題1
+    for word in sources:
+        if word == input_word:
+            print("{}が見つかりました".format(input_word))
+            result = 1
+            break
+        else:
+            pass
+    if result == 1:
+        pass
+    else:
+        ### 課題2
+        print("{}が見つかりませんでした".format(input_word))
+        ### 課題３
+        print("{}をリストに追加します".format(input_word))
+        sources.append(input_word)
+        ### 課題3出力結果確認
+        #print(sources)
 
-    print("{}が見つかりした".format(word))
+### 課題4
+def csvAddList():
+    with open("data.csv") as f:
+        for row in csv.reader(f):
+            sources.extend(row)
+    ### 課題4出力結果確認
+    #print(sources)
 
 if __name__ == "__main__":
+    ### csvファイルを読み込み後、検索ソースを実行するように順番を修正
+    csvAddList()
     search()
